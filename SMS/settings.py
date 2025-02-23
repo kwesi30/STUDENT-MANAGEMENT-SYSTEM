5b663e920347bf77410164f216baa94062200c49
kwesi30 <152696094+kwesi30@users.noreply.github.com>
2025-02-23 22:39:13 +0000

    Update settings.py
    List Updated to Tuples

--- SMS/settings.py
+++ SMS/settings.py
@@ -17,11 +17,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG" ,cast=bool)
 
-ALLOWED_HOSTS =ALLOWED_HOSTS = [
+ALLOWED_HOSTS = (
     "student-management-system-dcri.onrender.com",
     "localhost",
     "127.0.0.1",
-]
+)
+
 
 
 
